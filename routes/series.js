d2bc8a9872e73acf45804746cba2c5af2a84d7a1
Jesús Gallego Irles <jesusgallego@outlook.com>
2015-11-03 14:06:22 +0100

    POST comentario

--- routes/series.js
+++ routes/series.js
@@ -376,4 +376,56 @@ router.get('/:id/temporada/:season/capitulo/:episode', function(req, res) {
 	}
 });
 
+router.post('/:id/temporada/:season/capitulo/:episode/comentario', function(req, res) {
+	var id = req.params.id;
+	var seasonId = req.params.season;
+	var episodeId = req.params.episode;
+	if (isNaN(id)) {
+		res.status(400).send("Error: El id de la serie no es un número");
+	}
+	else if (isNaN(seasonId)) {
+		res.status(400).send("Error: El id de la temporada no es un número");
+	}
+	else if (isNaN(episodeId)) {
+		res.status(400).send("Error: El id del capitulo no es un número");
+	}
+	else {
+		models.Serie.findById(id).then(function(serie){
+			if (serie) {
+				serie.getTemporadas().then(function (temporadas) {
+					var isSeason = false;
+					temporadas.forEach(function (temporada) {
+						if(temporada.dataValues.id == seasonId) {
+							isSeason = true;
+							temporada.getCapitulos().then(function (capitulos) {
+								var isEpisode = false;
+								capitulos.forEach(function (capitulo) {
+									if(capitulo.dataValues.id == episodeId) {
+										isEpisode = true;
+										models.Comentario.create({
+											comment: req.body.comment,
+											UsuarioId: req.body.user,
+											CapituloId: episodeId
+										}).then(function () {
+											res.status(201).send("Comentario creado correctamente");
+										})
+									}
+								});
+								if(!isEpisode)
+									res.status(404).send("El capítulo no existe o no pertenece a esta temporada");
+							});
+						}
+					});
+					if (!isSeason) {
+						res.status(404).send("La temporada no existe o no pertenece a esta serie");
+					}
+				});
+			}
+			else {
+				res.status(404).send("La serie no existe");
+			}
+		});
+	}
+});
+
 module.exports = router;
