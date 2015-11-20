c32d764486640a5ca22d8139859c610f54f57c54
Jesús Gallego Irles <jesusgallego@outlook.com>
2015-11-20 01:59:08 +0100

    Allow CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var series = require('./routes/series');
 var usuario = require('./routes/usuario');
@@ -10,6 +11,8 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use('/api/series', series);
 app.use('/api/usuario', usuario);
